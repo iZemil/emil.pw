c86c761d60d2768a03cb3d498949af3ad321d7c5
iZemil <3zemil@gmail.com>
2022-12-11 21:29:40 +0000

    Deploying to main from  @ 1f9350576555924fbe339ca453fb8f5f085652c8 🚀

---
+++ docs/_next/static/jVoNP20aJz5uXC29MRzEz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/f36c6662-831e1550dd847487.js","static/chunks/330-b137da23631d077c.js","static/chunks/pages/index-071be1cf9158a764.js"],"/_error":["static/chunks/pages/_error-15a0bbaa3c2350f1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
