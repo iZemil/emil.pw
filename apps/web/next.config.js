a1b48766ce2634f621fccd317fdcc34091e4a06c
zemil <3zemil@gmail.com>
2022-12-15 03:22:40 +0300

    fix: react-aui

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,5 @@
 // https://github.com/martpie/next-transpile-modules#readme
-const withTM = require('next-transpile-modules')(['react-aui']); // pass the modules you would like to see transpiled
+const withTM = require('next-transpile-modules')(['react-aui', 'react-syntax-highlighter']); // pass the modules you would like to see transpiled
 const { withNx } = require('@nrwl/next/plugins/with-nx');
 
 /**
@@ -17,6 +17,21 @@ const nextConfig = {
 	},
 	images: {
 		unoptimized: true,
+	},
+	webpack: (config, options) => {
+		config.module.rules.push({
+			test: /\.css$/,
+			use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+		});
+
+		if (config.module.generator?.asset?.filename) {
+			if (!config.module.generator['asset/resource']) {
+				config.module.generator['asset/resource'] = config.module.generator.asset;
+			}
+			delete config.module.generator.asset;
+		}
+
+		return config;
 	},
 };
 
--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,10 +1,10 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import * as React from 'react';
-import { AUIProvider, GlobalStyles, IAUI } from 'react-aui';
+import { AUIProvider, GlobalStyles, ITheme, color } from 'react-aui';
 
-const theme: IAUI = {
-	mode: 'light',
+const theme: ITheme = {
+	key: 'light',
 	colors: {
 		// good color palette:
 		// https://www.theverge.com/23438982/hp-victus-15-gaming-laptop-review-specs-price
@@ -27,11 +27,6 @@ const theme: IAUI = {
 		large: 20,
 	},
 	borderRadius: 4,
-	globalStyles: `
-		h1, h2, h3, h4 {
-			margin: 0;
-		}
-	`,
 };
 
 function CustomApp({ Component, pageProps }: AppProps) {
@@ -41,9 +36,42 @@ function CustomApp({ Component, pageProps }: AppProps) {
 				<title>Emil Zaripov</title>
 			</Head>
 
-			<AUIProvider themes={[theme]}>
+			<AUIProvider theme={theme}>
 				<main className="app">
-					<GlobalStyles />
+					<GlobalStyles normalize>
+						{(t) => `
+					* {
+						box-sizing: border-box;
+					}
+
+					body {
+						font-family: Arial, sans-serif;
+						background-color: ${color('bg')({ theme: t })};
+						color: ${color('text')({ theme: t })};
+						transition: 0.3s background;
+					}
+
+					${(() => {
+						let headers = '';
+						Array.from({ length: 5 }).forEach((_, index) => {
+							const header = index + 1;
+
+							headers += `
+								h${header} {
+									font-size: ${30 - 2 * header}px;
+									font-weight: 400;
+								}
+							`;
+						});
+
+						return headers;
+					})()}
+
+					ul, ol {
+						margin: 0;
+					}
+				`}
+					</GlobalStyles>
 
 					<Component {...pageProps} />
 				</main>
