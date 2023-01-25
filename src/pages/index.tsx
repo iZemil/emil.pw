5ebbb84f0395e3adcc5956f29e4c3cbc96b3a064
zemil <3zemil@gmail.com>
2023-01-25 14:27:34 +0300

    fix: head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
 
 import { Resume } from '../components/Resume';
 
 export default function Home() {
 	return (
 		<Layout title="Resume" description="Full-Stack Javascript Developer">
-			<head>
+			<Head>
 				<meta property="og:url" content="https://emil.pw" />
 				<meta property="og:image" content="https://emil.pw/img/emil.jpg" />
 				<meta property="og:type" content="website" />
 				<meta property="og:site_name" content="emil.pw" />
-			</head>
+			</Head>
 
 			<main>
 				<Resume />
